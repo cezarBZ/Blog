fd71d23070ade273422ad3df4ceecff33662342d
Cézar Bezerra <cezar30santos@gmail.com>
2024-12-14 18:53:44 -0300

    add get all posts endpoint

---
+++ Blog.Application/ViewModels/PostViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Blog.Application.ViewModels
+{
+    public class PostViewModel
+    {
+        public PostViewModel(string title, string content, string coverImageUrl, DateTime createdAt, DateTime? updatedAt)
+        {
+            Title = title;
+            Content = content;
+            CoverImageUrl = coverImageUrl;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+
+        public string Title { get; private set; }
+        public string Content { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? UpdatedAt { get; private set; }
+        public string CoverImageUrl { get; private set; }
+    }
+}
