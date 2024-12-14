3a8c4589612280dd5b0e196c31c765939907134c
Cézar Bezerra <cezar30santos@gmail.com>
2024-12-14 20:16:11 -0300

    Add get post by id endpoint

---
+++ Blog.Application/Queries/GetPostById/GetPostByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Application.Responses;
+using Blog.Application.ViewModels;
+using MediatR;
+
+namespace Blog.Application.Queries.GetPostById
+{
+    public class GetPostByIdQuery : IRequest<Response<PostViewModel>>
+    {
+        public GetPostByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
---
+++ Blog.Application/Queries/GetPostById/GetPostByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Blog.Application.Responses;
+using Blog.Application.ViewModels;
+using Blog.Domain.AggregatesModel.PostAggregate;
+using MediatR;
+
+
+namespace Blog.Application.Queries.GetPostById
+{
+    public class GetPostByIdQueryHandler : IRequestHandler<GetPostByIdQuery, Response<PostViewModel>>
+    {
+        private readonly IPostRepository repository;
+        public GetPostByIdQueryHandler(IPostRepository _repository)
+        {
+            repository = _repository;
+        }
+        public async Task<Response<PostViewModel>> Handle(GetPostByIdQuery request, CancellationToken cancellationToken)
+        {
+            var post = await repository.GetByIdAsync(request.Id);
+
+
+            if (post == null)
+            {
+                return Response<PostViewModel>.NotFound("Post not found");
+            }
+            var postViewModel = new PostViewModel(post.Title, post.Content, post.CoverImageUrl, post.CreatedAt, post.UpdatedAt);
+
+            return Response<PostViewModel>.Success(postViewModel);
+        }
+    }
+}
