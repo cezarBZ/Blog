4d7c4aed68f6270e0ceb3cf88c639b98ce1ee39b
Cézar Bezerra <cezar30santos@gmail.com>
2025-01-14 15:29:54 -0300

    feat: update getById endpoint to include comments in post response

---
+++ Blog.Application/ViewModels/CommentViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog.Application.ViewModels;
+
+public class CommentViewModel
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
