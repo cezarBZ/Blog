38bc0b95cc6af22394398948492658470cc2d2e1
Cézar Bezerra <cezar30santos@gmail.com>
2025-02-01 21:39:46 -0300

    feat: Implements endpoint to follow user

---
+++ Blog.Application/Commands/UserCommands/Follow/FollowUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Application.Responses;
+using MediatR;
+
+namespace Blog.Application.Commands.UserCommands.Follow
+{
+    public class FollowUserCommand : IRequest<Response<Unit>>
+    {
+        public FollowUserCommand(int followedId)
+        {
+            FollowedId = followedId;
+        }
+
+        public int FollowedId { get; }
+    }
+}
